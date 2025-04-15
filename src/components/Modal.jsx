8dfb132953593799af5289ec9d0d7f210736f567
Bryan Leonard <bryan@forcequit.io>
2025-04-15 08:55:06 -0400

    Incremental save

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,7 +2,7 @@ import { forwardRef, useImperativeHandle, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 const Modal = forwardRef(function Modal ({children, btnCaption}, ref) {
-	const cssDialog = "";
+	const cssDialog = "p-6 rounded-md";
 	const cssBtn = "mx-auto px-6 py-2 rounded-md bg-stone-700 text-stone-50 hover:bg-stone-950";
 	
 	const dialog = useRef()
@@ -17,7 +17,7 @@ const Modal = forwardRef(function Modal ({children, btnCaption}, ref) {
 	return createPortal(
 		<dialog ref={dialog} className={cssDialog}>
 			{children}
-			<form method="dialog">
+			<form method="dialog" className="flex jsuity-center items-center">
 				<button className={cssBtn}>{btnCaption}</button>
 			</form>
 		</dialog>
